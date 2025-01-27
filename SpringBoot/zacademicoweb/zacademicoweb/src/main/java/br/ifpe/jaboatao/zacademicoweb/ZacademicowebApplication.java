e86d4b40a1460ad81c3a4230972d91f459e1fef3
Efraim Negreiros <efraimjnegreiross@gmail.com>
2025-01-27 10:13:25 -0300

    Add files via upload

--- SpringBoot/zacademicoweb/zacademicoweb/src/main/java/br/ifpe/jaboatao/zacademicoweb/ZacademicowebApplication.java
+++ SpringBoot/zacademicoweb/zacademicoweb/src/main/java/br/ifpe/jaboatao/zacademicoweb/ZacademicowebApplication.java
@@ -2,8 +2,10 @@ package br.ifpe.jaboatao.zacademicoweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"br.ifpe.jaboatao.zacademicoweb.spring_boot","br.ifpe.jaboatao.zacademicoweb.model.dao"})
 public class ZacademicowebApplication {
 
 	public static void main(String[] args) {
